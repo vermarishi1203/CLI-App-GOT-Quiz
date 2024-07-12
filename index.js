f52f36c953fbe7ea39865562cb3eaeac1fc7bfd1
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-12 22:55:30 +0530

    added question list

--- index.js
+++ index.js
@@ -2,18 +2,53 @@
 
 const prompt = require("prompt-sync")({sigint: true});
 
-const userName = prompt("Hey GOT fan, What's your name? ");
-
 console.log("\n");
+
+const userName = prompt("Hey GOT fan, What's your name? ");
 
 console.log(`Welcome to the quiz ${userName}!`);
 
 console.log("\n");
 
-const question1 = "Who is the youngest of Ned Stark’s children? ";
-const answer1 = "Rickon";
+const arrayOfQuestions = [
+    {
+        question: "Who is the youngest of Ned Stark\’s children? ",
+        answer: "Rickon"
+    },{
+        question: "Which noble character joins the band of assassins called \"Faceless Men\" in Braavos? ",
+        answer: "Arya Stark"
+    },{
+        question: "Who had the nickname \"The Onion Knight\"? ",
+        answer: "Davos Seaworth"
+    },{
+        question: "Which former ranger of the Night\’s Watch led an army of wildlings as the \“King-Beyond-the-Wall\”? ",
+        answer: "Mance Rayder"
+    },{
+        question: "What are the \“house words\” of House Stark? ",
+        answer: "Winter is Coming"
+    },{
+        question: "Which character, also known as the Lightning Lord, gets continually resurrected by Thoros of Myr after he dies? ",
+        answer: "Beric Dondarrion"
+    },{
+        question: "What is the sigil of House Baratheon? ",
+        answer: "A crowned Stag"
+    },{
+        question: "What is the name of Arya Stark\’s sword? ",
+        answer: "Needle"
+    },{
+        question: "Grey Worm is the leader of which group? ",
+        answer: "The unsullied"
+    },{
+        question: "Who masterminded the plot to kill King Joffrey Baratheon? ",
+        answer: "Olenna Tyrell"
+    }
+];
 
-questionPopper(question1, answer1);
+for (let i = 0; i < arrayOfQuestions.length ; i++ ) {
+    
+    questionPopper( arrayOfQuestions[i].question , arrayOfQuestions[i].answer );
+
+};
 
 function questionPopper (question, answer) {
 
@@ -25,6 +60,7 @@ function questionPopper (question, answer) {
     else {
         console.log("Sorry, wrong answer.");
     }
+
     console.log("\n");
 
 }
