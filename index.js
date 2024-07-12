b2c331f16374d478a929de915ffe40f08f310fdf
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-12 22:20:56 +0530

    added functionality: ask question and check answer

--- index.js
+++ index.js
@@ -2,5 +2,32 @@
 
 const prompt = require("prompt-sync")({sigint: true});
 
-let name = prompt("Enter your name: ");
-console.log(`hello ${name}`);
+const userName = prompt("Hey GOT fan, What's your name? ");
+
+console.log("\n");
+
+console.log(`Welcome to the quiz ${userName}!`);
+
+console.log("\n");
+
+const question1 = "Who is the youngest of Ned Stark’s children? ";
+const answer1 = "Rickon";
+
+questionPopper(question1, answer1);
+
+function questionPopper (question, answer) {
+
+    const userAnswer = prompt(question);
+
+    if ( userAnswer === answer) {
+        console.log("You are right!");
+    }
+    else {
+        console.log("Sorry, wrong answer.");
+    }
+    console.log("\n");
+
+}
+
+
+
