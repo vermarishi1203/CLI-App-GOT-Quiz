2768d1c2e0b96d46bca329d75de489c6ffffcfdb
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-13 14:40:07 +0530

    added functionality: question difficulty

--- index.js
+++ index.js
@@ -15,50 +15,78 @@ let currentScore = 0;
 const arrayOfQuestions = [
     {
         question: "Who is the youngest of Ned Stark\’s children? ",
-        answer: "Rickon"
+        answer: "Rickon Stark",
+        difficulty: "Medium"
     },{
         question: "Which noble character joins the band of assassins called \"Faceless Men\" in Braavos? ",
-        answer: "Arya Stark"
+        answer: "Arya Stark",
+        difficulty: "Easy"
     },{
         question: "Who had the nickname \"The Onion Knight\"? ",
-        answer: "Davos Seaworth"
+        answer: "Davos Seaworth",
+        difficulty: "Medium"
     },{
         question: "Which former ranger of the Night\’s Watch led an army of wildlings as the \“King-Beyond-the-Wall\”? ",
-        answer: "Mance Rayder"
+        answer: "Mance Rayder",
+        difficulty: "Medium"
     },{
         question: "What are the \“house words\” of House Stark? ",
-        answer: "Winter is Coming"
+        answer: "Winter is Coming",
+        difficulty: "Easy"
     },{
         question: "Which character, also known as the Lightning Lord, gets continually resurrected by Thoros of Myr after he dies? ",
-        answer: "Beric Dondarrion"
+        answer: "Beric Dondarrion",
+        difficulty: "Hard"
     },{
         question: "What is the sigil of House Baratheon? ",
-        answer: "A crowned Stag"
+        answer: "A Crowned Stag",
+        difficulty: "Easy"
     },{
         question: "What is the name of Arya Stark\’s sword? ",
-        answer: "Needle"
+        answer: "Needle",
+        difficulty: "Medium"
     },{
         question: "Grey Worm is the leader of which group? ",
-        answer: "The unsullied"
+        answer: "The Unsullied",
+        difficulty: "Easy"
     },{
         question: "Who masterminded the plot to kill King Joffrey Baratheon? ",
-        answer: "Olenna Tyrell"
+        answer: "Olenna Tyrell",
+        difficulty: "Hard"
     }
 ];
 
 for (let i = 0; i < arrayOfQuestions.length ; i++ ) {
     
-    questionPopper( arrayOfQuestions[i].question , arrayOfQuestions[i].answer );
+    questionPopper( arrayOfQuestions[i] );
 
 };
 
-function questionPopper (question, answer) {
+function questionPopper ({ question, answer, difficulty }) {
+
+    console.log(`Difficulty: ${difficulty}`);
 
     const userAnswer = prompt(question);
-
-    if ( userAnswer === answer) {
+    
+    if ( userAnswer.trim().toUpperCase() === answer.trim().toUpperCase()) {
         
-        currentScore++;
+        switch ( difficulty ) {
+
+            case "Easy":
+                currentScore++;
+                break;
+
+            case "Medium":
+                currentScore += 2;
+                break;
+            
+            case "Hard":
+                currentScore += 3;
+                break;
+            
+            default:
+                currentScore++;
+        };
 
         console.log("You are right!");
         
@@ -68,6 +96,8 @@ function questionPopper (question, answer) {
     else {
         
         console.log("Sorry, wrong answer.");
+
+        console.log(`The correct answer is: ${answer}`);
 
         console.log(`Your current score is: ${currentScore}`);
     }
